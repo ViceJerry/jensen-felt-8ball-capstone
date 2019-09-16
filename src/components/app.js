60a6eccb76a032e67673f3e91f5f5d59fd989649
Jensen Felt <jensenfelt@gmail.com>
2019-09-16 14:42:44 -0600

    first

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>The Magic 8 Ball</h1>
+      </div>
+    );
+  }
+}
