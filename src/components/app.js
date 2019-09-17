0279c44fd5c99afa80078037e6ed3037116fe2ab
Jensen Felt <jensenfelt@gmail.com>
2019-09-17 16:17:06 -0600

    scss

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div className="app-wrapper">
         <h1>The Magic 8 Ball</h1>
       </div>
     );
